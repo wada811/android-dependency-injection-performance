274258f7807f2d804e2ff654b920e50bdcd13f03
wada811 <at.wada811@gmail.com>
2020-09-11 01:12:45 +0900

    Update

--- app/src/main/java/com/sloydev/dependencyinjectionperformance/App.kt
+++ app/src/main/java/com/sloydev/dependencyinjectionperformance/App.kt
@@ -3,14 +3,14 @@ package com.sloydev.dependencyinjectionperformance
 import com.sloydev.dependencyinjectionperformance.dagger2.createAppComponent
 import com.sloydev.dependencyinjectionperformance.dependencyproperty.DependencyPropertyModule
 import com.sloydev.dependencyinjectionperformance.kodein.kodeinModule
-import com.wada811.dependencyproperty.DependencyContext
 import com.wada811.dependencyproperty.DependencyModules
+import com.wada811.dependencyproperty.DependencyModulesHolder
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 import org.kodein.di.DI
 import org.kodein.di.DIAware
 
-class App : DaggerApplication(), DependencyContext, DIAware {
+class App : DaggerApplication(), DependencyModulesHolder, DIAware {
     override val dependencyModules: DependencyModules by dependencyModules(DependencyPropertyModule())
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         val component = createAppComponent()
